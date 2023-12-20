1cc65bef80ea4771306105d01681ead1ce1fcdd4
mrvxk <kleinermarvin@icloud.com>
2023-12-20 10:28:31 +0100

    Enhance editing of list

--- todo-ui/src/components/ToDoList.vue
+++ todo-ui/src/components/ToDoList.vue
@@ -7,60 +7,62 @@
           class="todo-item"
       >
         <div class="todo-content">
-          <input
-              v-model="todo.name"
-              @blur="updateTodo(todo)"
-              class="todo-input"
-          />
-          <p class="todo-subtitle">Lorem ipsum dolor sit amet consectetur adipisicing elit</p>
+          <div v-if="isEditing(todo.id)" class="todo-edit-fields">
+            <input
+                v-model="todo.name"
+                class="todo-input"
+                @keyup.enter="updateTodo(todo)"
+                @blur="stopEditing(todo.id)"
+            />
+            <textarea
+                v-model="todo.notes"
+                class="todo-textarea"
+                @keyup.enter="updateTodo(todo)"
+                @blur="stopEditing(todo.id)"
+            ></textarea>
+          </div>
+          <div v-else class="todo-display-fields" @click="startEditing(todo.id)">
+            <h3 class="todo-title">{{ todo.name }}</h3>
+            <p class="todo-subtitle">{{ todo.notes }}</p>
+          </div>
         </div>
-        <button @click="deleteTodo(todo.id)">Delete</button>
+        <button @click="deleteTodo(todo.id)" class="delete-btn">Delete</button>
       </v-list-item>
     </v-list>
   </div>
 </template>
 
-<style>
-.todo-list {
-  width: 100vw;
-  padding: 20px;
-}
 
-.todo-item {
-  display: flex;
-  align-items: center;
-  justify-content: space-between;
-}
-
-.todo-content {
-  flex-grow: 1;
-}
-
-.todo-input {
-  width: 100%;
-  margin-right: 20px;
-}
-
-.todo-subtitle {
-  margin-top: 5px;
-}
-</style>
 
 <script setup lang="ts">
-import {ref, onMounted} from 'vue';
-import {Api, TodoResource} from "@/api/TodoService";
+import { ref, reactive, onMounted } from 'vue';
+import { Api, TodoResource } from "@/api/TodoService";
 
-const api = new Api({baseUrl: 'http://localhost:8080'});
+const api = new Api({ baseUrl: 'http://localhost:8080' });
 let data = ref<TodoResource[]>([]);
+let editingStatus = reactive(new Map<string, boolean>());
 
 onMounted(async () => {
   const response = await api.todos.todosList();
   data.value = response.data;
+  console.log(response.data)
+  data.value.forEach(todo => editingStatus.set(todo.id!, false));
 });
+
+const isEditing = (id: string) => editingStatus.get(id);
+
+const startEditing = (id: string) => {
+  editingStatus.set(id, true);
+};
+
+const stopEditing = (id: string) => {
+  editingStatus.set(id, false);
+};
 
 const updateTodo = async (todo: TodoResource) => {
   if (todo.id) {
     await api.todos.todosUpdate(todo);
+    stopEditing(todo.id);
   }
 };
 
@@ -69,3 +71,71 @@ const deleteTodo = async (id: string) => {
   data.value = data.value.filter(todo => todo.id !== id);
 };
 </script>
+
+
+<style>
+.todo-list {
+  max-width: 80vw;
+  margin: auto;
+  padding: 20px;
+}
+
+.todo-item {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  border-bottom: 1px solid #eee;
+  padding: 10px 0;
+}
+
+.todo-content {
+  flex-grow: 1;
+  display: flex;
+}
+
+.todo-edit-fields, .todo-display-fields {
+  flex-grow: 1;
+}
+
+.todo-input, .todo-textarea {
+  width: 100%;
+  margin-right: 20px;
+  box-sizing: border-box;
+}
+
+.todo-title {
+  margin: 0;
+  font-weight: bold;
+  margin-right: 10px;
+}
+
+.todo-subtitle {
+  margin: 0;
+}
+
+.delete-btn {
+  background-color: #ff4d4d;
+  color: white;
+  border: none;
+  padding: 10px 15px;
+  border-radius: 5px;
+  cursor: pointer;
+}
+
+.delete-btn:hover {
+  background-color: #ff3333;
+}
+
+.todo-input, .todo-textarea {
+  width: 100%;
+  margin-right: 20px;
+  box-sizing: border-box;
+  border: 1px solid transparent; /* Standard-Border, unsichtbar */
+  padding: 8px; /* Etwas Padding für bessere Lesbarkeit */
+}
+
+/* Stile für die Eingabefelder im Bearbeitungsmodus */
+.todo-input:focus, .todo-textarea:focus {
+  border: 1px solid black;
+}
+</style>
